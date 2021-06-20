1f69ac6a0154e678a5a7648e7796e2eb6d631e4c
AccidentalRebel <accidentalrebel@gmail.com>
2021-06-20 14:46:40 +0800

    Finished Day 2.

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -36,6 +36,23 @@ MainWindowProcCallback(HWND window,
 	 {
 		 OutputDebugStringA("WM_DESTROY\n");
 	 } break;
+
+	 case WM_PAINT:
+	 {
+		 PAINTSTRUCT paint;
+		 HDC deviceContext = BeginPaint(window, &paint);
+		 int x = paint.rcPaint.left;
+		 int y = paint.rcPaint.top;
+		 int width = paint.rcPaint.right - paint.rcPaint.left;
+		 int height = paint.rcPaint.bottom - paint.rcPaint.top;
+		 static DWORD operation = WHITENESS;
+		 if ( operation == WHITENESS )
+			 operation = BLACKNESS;
+		 else
+			 operation = WHITENESS;
+		 PatBlt(deviceContext, x, y, width, height, operation);
+		 EndPaint(window, &paint);
+	 } break;
 	 
 	 default:
 	 {
@@ -53,16 +70,53 @@ WinMain(HINSTANCE hInstance,
 				LPSTR lpCmdLine,
 				int nCmdShow)
 {
-	MessageBox(0, "This is Handmade Hero.", "Handmade Hero",
-						 MB_OK|MB_ICONINFORMATION);
-
 	WNDCLASSA windowClass = {};
-
 	windowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
 	windowClass.lpfnWndProc = MainWindowProcCallback;
   windowClass.hInstance = hInstance;
-  // windowClass.hIcon = ;
   windowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+	if ( RegisterClass(&windowClass) )
+	{
+		HWND windowHandle = CreateWindowExA(
+			0,
+			windowClass.lpszClassName,
+			"Handmade Hero",
+			WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			0,
+			0,
+			hInstance,
+			0);
+		if ( windowHandle )
+		{
+			for(;;)
+			{
+				MSG message;
+				BOOL messageResult = GetMessage(&message, 0, 0, 0);
+				if ( messageResult > 0 )
+				{
+					TranslateMessage(&message);
+					DispatchMessage(&message);
+				}
+				else
+				{
+					break;
+				}
+			}
+		} 
+		else
+		{
+			//TODO: Log error
+		}
+	}
+	else
+	{
+		// TODO: Log error
+	}
 	
 	return(0);
 }
